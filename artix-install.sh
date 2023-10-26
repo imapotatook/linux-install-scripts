f2b1bf4b9feedd03aa3c25d02e3c310f37ea687e
ImaPotatoOk <imapotatook123@gmail.com>
2023-10-26 19:10:23 +1030

    fix: update chaotic aur keys

--- artix-install.sh
+++ artix-install.sh
@@ -373,9 +373,11 @@ function chaotic-aur {
 	if [[ $chaotic =~ ([nN][oO]|[nN])$ ]]; then
 	cont 
 	else
-	    artix-chroot /mnt bash -c "pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com && pacman-key --lsign-key FBA220DFC880C036 && exit" 
+	    artix-chroot /mnt bash -c "pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com && pacman-key --lsign-key 3056513887B78AEB && exit" 
 		artix-chroot /mnt bash -c "pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' && exit"
-		artix-chroot /mnt bash -c "echo '[chaotic-aur]' >> /etc/pacman.conf && echo Include = '/etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf && pacman -Sy && exit"
+		artix-chroot /mnt bash -c "echo '
+		[chaotic-aur]' >> /etc/pacman.conf && echo '
+		Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf && pacman -Sy && exit"
 	fi
 	cont
 }
